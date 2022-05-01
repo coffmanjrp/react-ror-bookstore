a3b2eb738521493acbdacbf89f89b8624b4a4869
coffmanjrp <coffmanjrp@gmail.com>
2022-04-30 18:31:51 -0700

    Creating The React JS App for our Rails 7 API

---
+++ bookable/src/App.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import Books from './components/Books';
+import './App.css';
+
+const API_URL = 'http://localhost:3000/api/books';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
