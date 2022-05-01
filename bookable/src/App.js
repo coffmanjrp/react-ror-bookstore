9b47d827cfc2e16c7d083264101b112222890b9d
coffmanjrp <coffmanjrp@gmail.com>
2022-04-30 18:44:40 -0700

    Access to Rails 7 API From Javascript with Axios

--- bookable/src/App.js
+++ bookable/src/App.js
@@ -1,13 +1,30 @@
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Books from './components/Books';
 import './App.css';
 
-const API_URL = 'http://localhost:3000/api/books';
+const API_URL = 'http://localhost:3000/api/v1/books';
+
+const getAPIData = async () => {
+  const response = await axios.get(API_URL);
+  const data = response.data;
+
+  return data;
+};
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    getAPIData().then((items) => mounted && setBooks(items));
+    return () => (mounted = false);
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Books books={books} />
     </div>
   );
 }
--- bookable/src/components/Books.js
+++ bookable/src/components/Books.js
@@ -1,5 +1,15 @@
-const Books = () => {
-  return <div>Books</div>;
+const Books = ({ books }) => {
+  return (
+    <div>
+      <h1>These books are from the API</h1>
+      {books.map((book) => (
+        <div key={book.id}>
+          <h2>{book.title}</h2>
+          <p>{book.body}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Books;
